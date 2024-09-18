d6ba32028006a87530714a630071969322aa3716
aledev <herehk>
2024-09-18 21:18:28 +0200

    Version 1.0 - PRUEBA

--- app.py
+++ app.py
@@ -7,33 +7,59 @@ class ImageProcessor:
         self.imagen = Image.open(image_path)
         self.resultado = ""
 
-    def generarImagen(self):
-        # Recorre el alto (eje y)
-        for y in range(self.imagen.size[1]): 
-            # Recorre el ancho (eje x)
-            for x in range(self.imagen.size[0]):  
-                # Obtener el color del píxel en las coordenadas (x, y)
-                pixel = self.imagen.getpixel((x, y))
-                
-                # Comparar si el píxel es más claro o más oscuro que el umbral
-                if all(c >= 127.5 for c in pixel[:3]):  # Comparar solo R, G, B
-                    self.resultado += "#"
-                elif all(c > 0 for c in pixel[:3]) and all(c < 127.5 for c in pixel[:3]):
-                    self.resultado += "$"
-                else:
-                   self.resultado += " "
-            # Añadir un salto de línea al final de cada fila
+    def generarImagen(self, numIter):
+        # Convertir la imagen a escala de grisas
+        self.imagen = self.imagen.convert('L')
+
+        # Obtener el ancho y alto de la imagen
+        ancho, alto = self.imagen.size
+
+        thresholds = {
+            255: "@",   #  255-224 (very light gray)
+            224: "#",   #  223-208 (light gray)
+            208: "$",   #  207-192 (medium light gray)
+            192: "%",   #  191-176 (medium gray)
+            176: "*",   #  175-160 (medium dark gray)
+            160: "+",   #  159-144 (dark gray)
+            144: "-",   #  143-128 (very dark gray)
+            128: "█",   #  127-96 (black)
+            96: "▓",    #  95-80 (dark gray)
+            80: "▒",    #  79-64 (medium gray)
+            64: "░",    #  63-48 (medium light gray)
+            48: "·",    #  47-32 (light gray)
+            32: "~"     #  31-0 (very light gray)
+        }
+
+        for y in range(alto):
+            for x in range(ancho):
+                pixel = self.imagen.getpixel((x + numIter, y + numIter))
+                for threshold, char in thresholds.items():
+                    if pixel >= threshold:
+                        self.resultado += char
+                        break
+                    elif pixel < 32:  # add a default case for pixels with value < 32
+                        self.resultado += " "
+                        break
+
             self.resultado += "\n"
 
+
+
+
+
     def copiarResultado(self):
-        # Mostrar el resultado
-        print("El resultado ha sido copiado en el portapapeles")
+        # Copiar el resultado al portapapeles
         pyperclip.copy(self.resultado)
+        print("El resultado ha sido copiado en el portapapeles")
 
     def resultadoEnArchivo(self, filename):
         # Guardar el resultado en un archivo
         with open(filename, "w") as f:
             f.write(self.resultado)
         print("El resultado ha sido guardado en el archivo")
-
     
+    def imprimirResultado(self):
+        # Imprimir el resultado en la consola
+        print(self.resultado)
+    
+
--- imageAscier.py
+++ imageAscier.py
@@ -1,11 +1,14 @@
 from app import ImageProcessor
 
 def main():
-    image_path = "/home/aledev/Downloads/gl.png"
+    image_path = "./gl.png"
     image_processor = ImageProcessor(image_path)
-    image_processor.generarImagen()
-    image_processor.copiarResultado()
-    image_processor.resultadoEnArchivo("resultado2.txt")
+    image_processor.generarImagen(numIter=0) #Se declara numIter en 0 para que comience a analizar la imagen en las cordenadas (0,0)
+
+    #MOSTRAR RESULTADOS
+    #image_processor.copiarResultado()
+    image_processor.resultadoEnArchivo("resultado.txt")
+    #image_processor.imprimirResultado()
 
 if __name__ == "__main__":
     main()
