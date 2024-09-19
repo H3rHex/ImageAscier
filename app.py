91497393f2e5601dbfd7e09f0f88deaeea33c785
HEREHK-DEV <149667352+hereHK@users.noreply.github.com>
2024-09-19 09:27:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,32 +7,17 @@ class ImageProcessor:
         self.imagen = Image.open(image_path)
         self.resultado = ""
 
-    def generarImagen(self, numIter):
+    def generarImagen(self, thresholds):
         # Convertir la imagen a escala de grisas
         self.imagen = self.imagen.convert('L')
 
         # Obtener el ancho y alto de la imagen
         ancho, alto = self.imagen.size
-
-        thresholds = {
-            255: "@",   #  255-224 (very light gray)
-            224: "#",   #  223-208 (light gray)
-            208: "$",   #  207-192 (medium light gray)
-            192: "%",   #  191-176 (medium gray)
-            176: "*",   #  175-160 (medium dark gray)
-            160: "+",   #  159-144 (dark gray)
-            144: "-",   #  143-128 (very dark gray)
-            128: "█",   #  127-96 (black)
-            96: "▓",    #  95-80 (dark gray)
-            80: "▒",    #  79-64 (medium gray)
-            64: "░",    #  63-48 (medium light gray)
-            48: "·",    #  47-32 (light gray)
-            32: "~"     #  31-0 (very light gray)
-        }
+        
 
         for y in range(alto):
             for x in range(ancho):
-                pixel = self.imagen.getpixel((x + numIter, y + numIter))
+                pixel = self.imagen.getpixel((x , y))
                 for threshold, char in thresholds.items():
                     if pixel >= threshold:
                         self.resultado += char
@@ -44,9 +29,6 @@ class ImageProcessor:
             self.resultado += "\n"
 
 
-
-
-
     def copiarResultado(self):
         # Copiar el resultado al portapapeles
         pyperclip.copy(self.resultado)
@@ -54,12 +36,10 @@ class ImageProcessor:
 
     def resultadoEnArchivo(self, filename):
         # Guardar el resultado en un archivo
-        with open(filename, "w") as f:
+        with open(filename, "w", encoding="utf-8") as f:
             f.write(self.resultado)
         print("El resultado ha sido guardado en el archivo")
     
     def imprimirResultado(self):
         # Imprimir el resultado en la consola
         print(self.resultado)
-    
-
